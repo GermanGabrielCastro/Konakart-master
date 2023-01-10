f387228ff36578d99231d6f6cbcf4a74ce4747ab
gabriel <arrietagcastro@gmail.com>
2023-01-10 12:52:02 -0500

    Se sube el codigo fuente

---
+++ src/main/java/com/CustomTilesInitialitzer.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.apache.tiles.TilesApplicationContext;
+import org.apache.tiles.factory.AbstractTilesContainerFactory;
+import org.apache.tiles.startup.AbstractTilesInitializer;
+
+
+// edit for verita-204
+public class CustomTilesInitialitzer extends AbstractTilesInitializer {
+
+	@Override
+	protected AbstractTilesContainerFactory createContainerFactory(
+			TilesApplicationContext context) {
+		// TODO Auto-generated method stub
+		//for change coupling test
+		  return new CustomTilesContainerFactory();
+	}
+
+	// for change coupling by8 puneeth
+}
